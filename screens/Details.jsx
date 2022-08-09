3a77daf7e223dc1957ce4b1b14be3afd21eea6a9
FungaiPfende <fungaip163@outlook.com>
2022-08-09 19:13:59 +0200

    Set up basic Detail screen.

--- screens/Details.jsx
+++ screens/Details.jsx
@@ -14,17 +14,64 @@ import {
   CircleButton,
   RectButton,
   SubInfo,
-  FocusStatusBar,
+  FocusedStatusBar,
   DetailsDesc,
   DetailsBid,
 } from "../components";
 
-export const Details = () => {
+export const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+
+      <View style={styles.buttonContainer}>
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.contentContainer}
+      />
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  buttonContainer: {
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+    paddingVertical: SIZES.font,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    zIndex: 1,
+  },
+
+  contentContainer: {
+    paddingBottom: SIZES.extraLarge * 3,
+  },
+
+  detailsHeader: {
+    width: "100%",
+    height: 373,
+  },
+
+  headerImage: {
+    width: "100%",
+    height: "100%",
+  },
+});
